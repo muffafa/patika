13d588277c3455762dc39564b31d941d9f91444a
muffafa <mm.savar@gmail.com>
2023-04-19 18:21:48 +0300

    fs module imported

---
+++ nodejs/test/fs/index.js
@@ -0,0 +1,30 @@
+import * as fs from 'node:fs';
+
+// fs.writeFile('./password.txt', 'deneme',(err) => {
+//     if (err) throw err;
+// });
+
+// fs.readFile('./password.txt', 'utf8', (err, data) => {
+//     if (err) throw err;
+//     else console.log(data);
+// });
+
+// fs.appendFile('./password.txt', '\ndeneme2', (err) => {
+//     if (err) throw err;
+//     else console.log("added")
+// });
+
+// fs.unlink('./password.txt', (err) => {
+//     if (err) throw err;
+//     else console.log("removed")
+// });
+
+// fs.mkdir('public/img', { recursive: true }, (err) => {
+//     if (err) throw err;
+//     else console.log("Files created:");
+// });
+
+// fs.rmdir('./public/img', (err) => {
+//     if (err) throw err;
+//     else console.log("Files deleted:");
+// });
