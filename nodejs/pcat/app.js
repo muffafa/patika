12b70f85db95ba346dd287ed7d5e6337f94c6692
muffafa <mm.savar@gmail.com>
2023-04-20 04:08:49 +0300

    pcat init

--- nodejs/pcat/app.js
+++ nodejs/pcat/app.js
@@ -1,3 +1,11 @@
-function deneme() {
-  console.log('deneme')
-}
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
+})
