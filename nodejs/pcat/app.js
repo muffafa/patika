9577a5bd022a5b81d641186dbb4270fe58ec29c9
muffafa <mm.savar@gmail.com>
2023-04-20 04:32:32 +0300

    static files added

--- nodejs/pcat/app.js
+++ nodejs/pcat/app.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+// const myLogger = (req, res, next) => {
+//     console.log('logging 1')
+//     next()
+// }
+
+app.use(express.static('public'))
+// app.use(myLogger)
+
 app.get('/', (req, res) => {
-    res.send('hello world')
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
 })
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`)
+  console.log(`listening on ${port}`)
 })
