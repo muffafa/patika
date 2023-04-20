023bbeb6b511b8fba0e61a1ff9f25cbfca0457c4
muffafa <mm.savar@gmail.com>
2023-04-20 18:20:43 +0300

    pcat template engine

--- nodejs/pcat/app.js
+++ nodejs/pcat/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const ejs = require('ejs')
 const path = require('path')
+
 const app = express()
 const port = 3000
+
+app.set('view engine', 'ejs')
 
 // const myLogger = (req, res, next) => {
 //     console.log('logging 1')
@@ -12,7 +16,18 @@ app.use(express.static('public'))
 // app.use(myLogger)
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  res.render('about')
+})
+
+app.get('/add', (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  res.render('add')
 })
 
 app.listen(port, () => {
