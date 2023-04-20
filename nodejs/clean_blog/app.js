75178a01cf880a43cb25464d0ccd32f0d34c03f8
muffafa <mm.savar@gmail.com>
2023-04-20 18:21:14 +0300

    Ödev 8

--- nodejs/clean_blog/app.js
+++ nodejs/clean_blog/app.js
@@ -2,9 +2,26 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
+    // const blog = { id: 1, title: "Blog title", description: "Blog description" }
+    // res.send(blog)
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
+})
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
 })
 
 app.listen(port, () => {
