43300b90b494f63ef184c2e66edc60f4d0cdc945
muffafa <mm.savar@gmail.com>
2023-04-23 02:29:56 +0300

    blogs readed rendered and added to database

--- nodejs/clean_blog/app.js
+++ nodejs/clean_blog/app.js
@@ -1,15 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+const ejs = require('ejs')
+const path = require('path')
+
+const Post = require('./models/Post')
+
 const app = express()
 const port = 4000
+
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db')
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     // const blog = { id: 1, title: "Blog title", description: "Blog description" }
     // res.send(blog)
-    res.render('index')
+    const posts = await Post.find({})
+    res.render('index', {
+        posts
+    })
 })
 
 app.get('/about', (req, res) => {
@@ -22,6 +37,12 @@ app.get('/post', (req, res) => {
 
 app.get('/add_post', (req, res) => {
     res.render('add_post')
+})
+
+app.post('/blogs', async (req, res) => {
+    //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+    await Post.create(req.body)
+    res.redirect('/')
 })
 
 app.listen(port, () => {
