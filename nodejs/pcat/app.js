d5b4aa358b0e3e6f2afa46c29d3762949a48eff0
muffafa <mm.savar@gmail.com>
2023-04-23 02:00:16 +0300

    connect db - add photo - render from db

--- nodejs/pcat/app.js
+++ nodejs/pcat/app.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
 const ejs = require('ejs')
 const path = require('path')
 
+const Photo = require('./models/Photo')
+
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://localhost/pcat-test-db')
 
 app.set('view engine', 'ejs')
 
@@ -13,11 +19,16 @@ app.set('view engine', 'ejs')
 // }
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
 // app.use(myLogger)
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-  res.render('index')
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  })
 })
 
 app.get('/about', (req, res) => {
@@ -28,6 +39,12 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
   res.render('add')
+})
+
+app.post('/photos', async (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  await Photo.create(req.body)
+  res.redirect('/')
 })
 
 app.listen(port, () => {
