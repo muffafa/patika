80044266ee1e51f002cab55895519837310b0277
muffafa <mm.savar@gmail.com>
2023-04-23 03:46:46 +0300

    each post rendered

--- nodejs/clean_blog/app.js
+++ nodejs/clean_blog/app.js
@@ -27,6 +27,13 @@ app.get('/', async (req, res) => {
     })
 })
 
+app.get('/posts/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    res.render('post', {
+        post
+    })
+})
+
 app.get('/about', (req, res) => {
     res.render('about')
 })
