531e97cad115c42fa50a4525ea8c9ae7e9254e64
muffafa <mm.savar@gmail.com>
2023-04-23 05:46:02 +0300

    edit a photo

--- nodejs/pcat/app.js
+++ nodejs/pcat/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const fileUpload = require('express-fileupload')
+const methodOverride = require('method-override')
 
 const ejs = require('ejs')
 const path = require('path')
+const fs = require('fs')
 
 const Photo = require('./models/Photo')
 
@@ -19,23 +22,25 @@ app.set('view engine', 'ejs')
 // }
 
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(fileUpload())
+app.use(methodOverride('_method'))
 // app.use(myLogger)
 
 app.get('/', async (req, res) => {
   //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-  const photos = await Photo.find({})
+  const photos = await Photo.find({}).sort('-dateCreated')
   res.render('index', {
-    photos
+    photos,
   })
 })
 
 app.get('/photos/:id', async (req, res) => {
   //console.log(req.params.id)
   const photo = await Photo.findById(req.params.id)
-  res.render('photo', { 
-    photo
+  res.render('photo', {
+    photo,
   })
 })
 
@@ -51,8 +56,39 @@ app.get('/add', (req, res) => {
 
 app.post('/photos', async (req, res) => {
   //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-  await Photo.create(req.body)
-  res.redirect('/')
+
+  const uploadDir = 'public/uploads'
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir)
+  }
+
+  let uploadedImage = req.files.image
+  let uploadedImagePath = __dirname + '/public/uploads/' + uploadedImage.name
+
+  uploadedImage.mv(uploadedImagePath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
+    })
+    res.redirect('/')
+  })
+})
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id)
+  res.render('edit', {
+    photo,
+  })
+})
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id)
+  photo.title = req.body.title
+  photo.description = req.body.description
+  photo.save()
+
+  res.redirect(`/photos/${req.params.id}`)
 })
 
 app.listen(port, () => {
