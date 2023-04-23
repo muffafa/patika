96df04130355e5ea7495e95c11860125a40dcfa0
muffafa <mm.savar@gmail.com>
2023-04-23 09:47:23 +0300

    pagination added and page header title changed

--- nodejs/pcat/controllers/photoController.js
+++ nodejs/pcat/controllers/photoController.js
@@ -1,54 +1,65 @@
-const Photo = require('../models/Photo');
+const Photo = require('../models/Photo')
 const fs = require('fs')
 
 exports.getAllPhotos = async (req, res) => {
-    const photos = await Photo.find({}).sort('-dateCreated')
-    res.render('index', {
-      photos,
-    })
+  const pageNumber = req.query.page ?? 1
+  const photosPerPage = 3
+
+  const totalPhotos = await Photo.find().countDocuments()
+
+  const photos = await Photo.find({})
+  .sort('-dateCreated')
+  .skip((pageNumber-1) * photosPerPage)
+  .limit(photosPerPage)
+
+  res.render('index', {
+    photos: photos,
+    currentPage: pageNumber,
+    totalPageNumber: Math.ceil(totalPhotos / photosPerPage)
+  })
 }
 
 exports.getPhoto = async (req, res) => {
-    //console.log(req.params.id)
-    const photo = await Photo.findById(req.params.id)
-    res.render('photo', {
-      photo,
-    })
+  //console.log(req.params.id)
+  const photo = await Photo.findById(req.params.id)
+  res.render('photo', {
+    photo,
+  })
+}
+
+exports.createPhoto = async (req, res) => {
+  const uploadDir = 'public/uploads'
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir)
   }
 
-exports.createPhoto = async (req, res) => {  
-    const uploadDir = 'public/uploads'
-  
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir)
-    }
-  
-    let uploadedImage = req.files.image
-    let uploadedImagePath = __dirname + '/../public/uploads/' + uploadedImage.name
-  
-    uploadedImage.mv(uploadedImagePath, async () => {
-      await Photo.create({
-        ...req.body,
-        image: '/uploads/' + uploadedImage.name,
-      })
-      res.redirect('/')
+  let uploadedImage = req.files.image
+  let uploadedImagePath = __dirname + '/../public/uploads/' + uploadedImage.name
+
+  uploadedImage.mv(uploadedImagePath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
     })
-  }
+    res.redirect('/')
+  })
+}
 
 exports.updatePhoto = async (req, res) => {
-    const photo = await Photo.findById(req.params.id)
-    photo.title = req.body.title
-    photo.description = req.body.description
-    photo.save()
-  
-    res.redirect(`/photos/${req.params.id}`)
-  }
+  const photo = await Photo.findById(req.params.id)
+  photo.title = req.body.title
+  photo.description = req.body.description
+  photo.save()
+
+  res.redirect(`/photos/${req.params.id}`)
+}
 
 exports.deletePhoto = async (req, res) => {
-    const photo = await Photo.findById(req.params.id)
-    let imagePath = __dirname + '/../public' + photo.image
-    fs.unlinkSync(imagePath);
-    await Photo.findByIdAndRemove(req.params.id)
-  
-    res.redirect("/")
-  }
+  const photo = await Photo.findById(req.params.id)
+  let imagePath = __dirname + '/../public' + photo.image
+  fs.unlinkSync(imagePath)
+  await Photo.findByIdAndRemove(req.params.id)
+
+  res.redirect('/')
+}
